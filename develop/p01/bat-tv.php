e9bb48500a301563ff539f5a04b045ae5b02b286
DST2000 <dnkst1c@rambler.ru>
2020-07-10 09:28:48 +0300

    bat-tv

---
+++ develop/p01/bat-tv.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=3.0, user-scalable=yes"/>
+	<meta name="HandheldFriendly" content="true"/>
+	<meta name="apple-mobile-web-app-capable" content="YES"/>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;900&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="css/style-tv.css">
+</head>
+
+<body>
+	<?php // Заготовка ?>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-12">
+				
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+	<ol class="carousel-indicators">
+		<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+		<li data-target="#myCarousel" data-slide-to="1" class=""></li>
+		<li data-target="#myCarousel" data-slide-to="2" class=""></li>
+	</ol>
+	<div class="carousel-inner">
+		<div class="carousel-item active">
+			<svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img">
+				<rect width="100%" height="100%" fill="#777"></rect>
+			</svg>
+			<div class="container">
+				<div class="carousel-caption text-left">
+					<h1>Сегодня</h1>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam, veritatis et vel. Deleniti dicta neque vel! Est animi neque, ab fuga aliquid placeat exercitationem possimus ipsum, debitis perferendis dolores voluptas eius tempore, praesentium veniam recusandae consectetur rem culpa accusamus? Ullam deleniti, obcaecati repudiandae praesentium, et amet quisquam perferendis architecto dignissimos ab esse debitis excepturi illum voluptatibus atque iusto sequi sed accusantium. Ipsa voluptatum, mollitia quo at quisquam esse voluptate aliquid eveniet corporis, cupiditate quas fugit assumenda molestiae doloremque laudantium consequuntur! Nostrum, eum atque ea temporibus maxime dolor porro esse doloremque officia delectus quis velit repellat earum recusandae vel amet fugiat incidunt quo distinctio, dignissimos saepe consequuntur adipisci voluptatum architecto. Sapiente nobis officiis eum dignissimos distinctio labore, dolorem enim, fuga veniam omnis, corporis vitae. Ducimus beatae deleniti accusantium eaque, vero placeat. Blanditiis repellat placeat qui ea quidem veniam quae ratione quibusdam, fugit dolorem quas sint, nesciunt, rerum unde modi, dignissimos doloremque? Laudantium, consequatur voluptate itaque perspiciatis fugit incidunt omnis laborum neque error in molestias voluptatem ipsum magni consequuntur sint delectus modi dicta quas animi. Error expedita quo a, non optio, eaque labore repellat dolore consequuntur esse quia tempora, unde molestiae ab asperiores eveniet perspiciatis. Officiis provident illum debitis explicabo repudiandae culpa, fuga doloribus dignissimos suscipit cumque corporis esse possimus aliquid nesciunt saepe eveniet animi illo, nostrum, soluta obcaecati rerum nihil aliquam at itaque eos! Saepe illo debitis esse praesentium quam fugit consectetur aperiam iure in at, facilis nulla cumque ducimus rerum, aliquid nobis consequatur quia, laboriosam beatae nostrum officiis sapiente odio. Necessitatibus sint incidunt magnam, nisi possimus, accusantium expedita laudantium! Temporibus officia quisquam aperiam nesciunt, explicabo fugiat fugit quo nihil voluptatum qui rem, distinctio quos, est, quia libero. Nostrum officiis quaerat facilis aliquid adipisci ab quod, explicabo fugit, dolor beatae libero ratione fuga harum, molestias accusamus voluptas iste, labore itaque porro?</p>
+					<p><a class="btn btn-lg btn-primary" href="#" role="button">Сегодня</a>
+					</p>
+				</div>
+			</div>
+		</div>
+		<div class="carousel-item">
+			<svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img">
+				<rect width="100%" height="100%" fill="#777"></rect>
+			</svg>
+			<div class="container">
+				<div class="carousel-caption">
+					<h1>Неделя</h1>
+					<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+					<p><a class="btn btn-lg btn-primary" href="#" role="button">Неделя</a>
+					</p>
+				</div>
+			</div>
+		</div>
+		<div class="carousel-item">
+			<svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img">
+				<rect width="100%" height="100%" fill="#777"></rect>
+			</svg>
+			<div class="container">
+				<div class="carousel-caption text-right">
+					<h1>Сравнение с предидущим месяцем</h1>
+					<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+					<p><a class="btn btn-lg btn-primary" href="#" role="button">Сравнение с предидущим месяцем</a>
+						
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+
+	<a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+
+</div>
+				
+				
+			</div>
+		</div>
+	</div>
+	<footer>
+	</footer>
+	<script src="js/jquery-3.5.1.slim.min.js"></script>	
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/popper.min.js"></script>
+</body>
+</html>	
